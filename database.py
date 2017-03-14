0c96ce9fc68a526c66ec96453d9d25287faa06e7
Everlyne <Everlyne>
2017-03-14 18:38:12 +0300

    My Database

---
+++ database.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import *
+
+
+Base = declarative_base()
+ 
+
+ 
+class Events(Base):
+    __tablename__ = 'create_events'
+    id = Column(Integer, primary_key=True)
+    event_name = Column(String(250), nullable=False)
+    event_start_date=Column(String(250), nullable=False)
+    event_end_date = Column(String(250), nullable=False)
+    event_venue = Column(String(250), nullable=False)
+
+class Tickets(Base):
+    __tablename__ = 'tickets'
+    id = Column(Integer, primary_key=True)
+    event_id = Column(Integer, ForeignKey('create_events.id'))
+    tickets = relationship('Events')
+    
+ 
+# Create an engine that stores data in the local directory's
+# sqlalchemy_example.db file.
+engine = create_engine('sqlite:///tickets.db')
+ 
+# Create all tables in the engine. This is equivalent to "Create Table"
+# statements in raw SQL.
+Base.metadata.create_all(engine)
